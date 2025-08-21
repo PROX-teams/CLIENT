4815a215733a2b05a4b5ace932c9c1ecd99e2bb4
김예안 <kjan0929@naver.com>
2025-08-21 23:23:07 +0900

    fix: 타입 및 import 오류 수정 [BETA-103]

--- src/features/main/ui/atoms/graph-nav-button/GraphNavButton.tsx
+++ src/features/main/ui/atoms/graph-nav-button/GraphNavButton.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link";
 import Button from "@/shared/ui/atoms/button/Button";
-import { ButtonHTMLAttributes } from "react";
+import { AnchorHTMLAttributes } from "react";
 import { ButtonBase } from "@/shared/types/components.types";
 import RoutingIcon from "@/shared/assets/icons/main/routing.svg";
 import * as S from "./GraphNavButton.css";
 
 interface NodeGraphNavButtonProps
   extends ButtonBase,
-    ButtonHTMLAttributes<HTMLButtonElement> {}
+    AnchorHTMLAttributes<HTMLAnchorElement> {}
 
 /**
  * Node Graph 페이지로 이동시키는 라우팅 버튼입니다.
